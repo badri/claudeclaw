1d2b0c8f6bee4f16fd5f3f2f9bd7a83d659068ce
moazbuilds <mo3azmuham@gmail.com>
2026-02-14 17:05:51 +0200

    set web page title to claudeclaw

--- src/ui/page/template.ts
+++ src/ui/page/template.ts
@@ -19,7 +19,7 @@ export function htmlPage(): string {
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>ClaudeClaw New Tab</title>
+  <title>ClaudeClaw</title>
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,300;9..144,500&family=Space+Grotesk:wght@400;500;700&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
